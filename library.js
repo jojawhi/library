78bd17dd26ee440daced1e3f35ddc111cbb78eba
Josh White <jojawhi@gmail.com>
2021-12-06 16:01:37 -0800

    completed createBookCard, createCheckmark, and displayLibrary functions. objects will now display on page. need to add form input, delete, and changeReadStatus and desktop layout styles

--- library.js
+++ library.js
@@ -16,7 +16,13 @@ function Book(title, author, pages, read) {
     }
 }
 
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "read");
+const sampleBook = new Book("The Hobbit", "J.R.R. Tolkien", "295", "read");
+
+const libraryContainer = document.querySelector('#libraryContainer');
+const bookTitleInput = document.querySelector('#bookTitleInput');
+const bookAuthorInput = document.querySelector('#bookAuthorInput');
+const bookPagesInput = document.querySelector('#bookPagesInput');
+const readStatusInput = document.querySelector('#readStatusInput');
 
 let libraryArray = [
     {
@@ -36,29 +42,122 @@ let libraryArray = [
         author: "Orson Scott-Card",
         pages: 324,
         read: true
+    },
+    {
+        title: "An Astronaut's Guide to Life",
+        author: "Chris Hadfield",
+        pages: 324,
+        read: false
     }
 ];
 
-function addBookToLibrary() {
+const addBookToLibrary = (title, author, pages, read) => {
     /*
     1. accept form input
     2. create new book object from form input
     3. add new book object to libraryArray
+    4. run displayBooks() to update display
     */
+
+    new Book (title, author, pages, read);
+
+
 }
 
-function displayBooks() {
+const displayBooks = () => {
 
     /*
     1. loop through libraryArray
-    2. for each book object, create a book div inside libraryContainer (querySelector needed)
-    3. for each book div created, create child elements
+    2. for each book object, run createBookCard()
+    3. append bookCards as children of libraryContainer
+
     */
+
+    for (i = 0; i < libraryArray.length; i++) {
+
+        createBookCard(libraryArray[i]);
+
+    }
+
+
+}
+
+const createBookCard = (book) => {
+
+    const bookCard = document.createElement('div');
+    const bookTitle = document. createElement('h1');
+    const bookAuthor = document.createElement('h3');
+    const bookPages = document.createElement('p');
+    const bookNumberOfPages = document.createElement('span');
+    const checkContainer = createCheckmark(book.read);
+    const bookCardRemoveButton = document.createElement('button');
+    const removeButtonImage = document.createElement('img');
+
+    bookCard.classList.add('book');
+    if (book.read === false) {
+        bookCard.classList.add('unreadBook')
+    } else {
+        bookCard.classList.add('readBook');
+    }
+    bookTitle.classList.add('title', 'text');
+    bookAuthor.classList.add('author', 'text');
+    bookPages.classList.add('pages', 'text', 'semibold');
+    bookNumberOfPages.classList.add('numberOfPages');
+    bookCardRemoveButton.classList.add('removeButton');
+    removeButtonImage.setAttribute('src', 'images/recycle.svg');
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = `Pages: ${book.pages}`;
+    
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookPages.appendChild(bookNumberOfPages);
+    bookCard.appendChild(checkContainer);
+    bookCard.appendChild(bookCardRemoveButton);
+    bookCardRemoveButton.appendChild(removeButtonImage);
+    libraryContainer.appendChild(bookCard);
+
+}
+
+const createCheckmark = (boolean) => {
+
+    const checkContainer = document.createElement('div');
+    const checkLabel = document.createElement('label');
+    const checkLabelText = document.createElement('span');
+    const checkbox = document.createElement('input');
+    const customCheckmark = document.createElement('span');
+
+    checkContainer.classList.add('readContainer');
+    checkLabel.classList.add('text', 'semibold', 'customCheckboxContainer');
+    checkLabelText.classList.add('labelText');
+    checkLabelText.textContent = "Read: ";
+    checkbox.setAttribute('type', 'checkbox');
+    if (boolean === true) {
+        checkbox.checked = true;
+    } else {
+        checkbox.checked = false;
+    }
+    checkbox.classList.add('readStatusTileCheckbox');
+    customCheckmark.classList.add('checkmark');
+
+    checkContainer.appendChild(checkLabel);
+    checkLabel.appendChild(checkLabelText);
+    checkLabel.appendChild(checkbox);
+    checkLabel.appendChild(customCheckmark);
+    
+    return checkContainer;
 
 }
 
 function removeBookFromLibrary() {
+    /*
+    
+    1. delete book object from libraryArray
+    2. run displayBooks() to update display
 
+    */
 }
 
 function changeReadStatus() {
@@ -67,150 +166,4 @@ function changeReadStatus() {
 
 console.log(libraryArray);
 
-// Prototypes
-
-// Prototype Property
-/*
-
-function PrintStuff (myDocuments) {
-    this.documents = myDocuments;
-}
-
-
-// add the print () method to PrintStuff () constructor so that other instances can inherit it
-
-PrintStuff.prototype.print = function () {
-    console.log(this.documents);
-}
-
-
-// Use PrintStuff () constructor to create a new object, which will inherit PrintStuff's properties and methods 
-
-const newObject = new PrintStuff ("I am a new object, and I can use the print method.");
-
-
-// newObject inherited the addition of the print method to PrintStuff's prototype, and can now print
-
-newObject.print(); // returns "I am a new object, and I can use the print method."
-
-*/
-
-// Constructors
-
-// In the above examples, PrintStuff () was the constructor for newObject
-
-/*
-
-function Account () {
-}
-
-// for userAccount, the constructor is Account ()
-const userAccount = new Account ();
-
-// to find the constructor:
-
-console.log(userAccount.constructor);
-
-*/
-
-
-// Prototype Attribute
-
-// making an object with the new keyword or a constructor give the object a prototype of constructorName.prototype
-
-/*
-function Account () {
-
-}
-
-let userAccount = new Account ();
-// Because the constructor is Account (), the prototype attribute or object will be Account.prototype
-*/
-
-
-// Prototype inheritance
-
-// you can assign multiple prototypes to an object if it fits into multiple categories, like fruit and plant
-
-/*
-
-function Plant () {
-    this.country = "Mexico";
-    this.isOrganic = true;
-}
-
-// Add showNameAndColor method to Plant prototype property
-Plant.prototype.showNameAndColor = function () {
-    console.log("I am a " + this.name + " and my color is " + this.color);
-}
-
-function Fruit (fruitName, fruitColor) {
-    this.name = fruitName;
-    this.color = fruitColor;
-}
-
-// Set Fruit's prototype to Plant's constructor, allowing Fruit to inherit all Plant.prototype's methods and properties
-Fruit.prototype = new Plant ();
-
-// Create a new object with Fruit () constructor
-var aBanana = new Fruit ("Banana", "Yellow");
-
-// aBanana can use the Fruit properties, like name
-console.log(aBanana.name); // returns Banana
-
-// aBanana can also use Plant's properties, like showNameAndColor, because Fruit constructor's prototype inheritance
-console.log(aBanana.showNameAndColor());
-
-*/
-
-// Prototype Inheritance exercises
-
-/*
-let head = {
-    glasses: 1
-};
-  
-let table = {
-    __proto__: head,
-    pen: 3
-};
-  
-let bed = {
-    __proto__: table,
-    sheet: 1,
-    pillow: 2
-};
-  
-let pockets = {
-    __proto__: bed,
-    money: 2000
-};
-
-console.log(bed.glasses);
-*/
-
-/*
-let hamster = {
-  
-    eat(food) {
-      this.stomach.push(food);
-    }
-};
-  
-let speedy = {
-    stomach: [],
-    __proto__: hamster
-};
-  
-let lazy = {
-    stomach: [],
-    __proto__: hamster
-};
-  
-// This one found the food
-speedy.eat("apple");
-console.log(speedy.stomach); // apple
-  
-// This one also has it, why? fix please.
-console.log(lazy.stomach); // apple
-*/
+displayBooks();
