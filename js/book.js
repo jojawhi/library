e43a538f18d0a113b15e0b3d068401e53c05f0c4
Jesse Smit <jesse@jessesmit.com>
2021-12-08 09:21:42 +0100

    refactor the Book object to a seperate class

---
+++ js/book.js
@@ -0,0 +1,40 @@
+// Class instead of functions. ES6 all the way!
+class Book {
+    // Later on when you will develop larger and more complex applications, you need to split your code up.
+    // Try to think of it as each file has their own "entity" or "responsibility". This book is an example of that.
+
+    constructor(title = null, author = null, totalPages = null, isRead = false) {
+        this.title = title
+        this.author = author
+        this.totalPages = totalPages // pages -> totalPages (makes it more clear what type of var we're dealing with)
+        this.isRead = false // read -> isRead (makes it more clear what type of var we're dealing with)
+
+        /*
+        This didn't make sense. It's the same as: this.read = read
+        this.read = function() {
+            if (read) {
+                return true;
+            } else {
+                return false;
+            }
+        },*/
+
+        // function() -> () => conversion
+        // Check: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+        this.info = () => `${title} by ${author}, ${pages} pages, ${read}`
+    }
+
+    // (Example method that you could later add)
+    addToLibrary(library) { // You can pass your Library object here "by reference" (https://www.javascripttutorial.net/javascript-pass-by-value/)
+        // (will not work now, but you get the idea)
+        const newBook = getBookFromInput()
+
+        if (library.find(({title}) => title === newBook.title)) {
+            alert('This book is already in your library.')
+        } else {
+            library.push(newBook)
+        }
+
+        updateLibraryDisplay()
+    }
+}
